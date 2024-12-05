c244f354bd5bbc923483e51272606bbb3f437b0f
Ernesto Fava <e.fava13@gmail.com>
2024-12-05 15:51:15 -0400

    Comprobando tipos
    Comprobando si los tipos funcionan correctamente con el script "dev".

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 
-app.get("/", (req:any, res:any) => {
+app.get("/", (req:Express.Request, res: { json: (arg0: string) => void; }) => {
   res.json("Hello World!");
 });
 
