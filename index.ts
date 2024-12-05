52fef6e248a38ae4349dea5969576b70e586e0a6
Ernesto Fava <e.fava13@gmail.com>
2024-12-05 16:23:50 -0400

    Módulos y configuración
    Creando módulos para comprobar funcionamiento y añadiendo valor a la configuración para incluir a index.ts en la transpilación.

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 const express = require('express');
+const routes = require('./src/routes/router');
 
 const app = express();
 const port = process.env.PORT || 3000
 
-app.get("/", (req:Express.Request, res: { json: (arg0: string) => void; }) => {
-  res.json("Hello World!");
-});
+app.get("*", routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ src/controllers/controller.ts
@@ -0,0 +1,11 @@
+const test = (req:Express.Request, res: { json: (arg0: { status: number; success: boolean; message: string; }) => void; }) => {
+    res.json({
+        status: 200,
+        success: true,
+        message: "Hello World"
+    });
+}
+
+module.exports = {
+    test
+};
