da155d652cd16253c46a354000eba82784e47c81
Ernesto Fava <e.fava13@gmail.com>
2024-12-06 01:38:02 -0400

    Uso de interface
    Se hizo la declaración de tipos para los controladores por medio de una interface para así deducir todas las propiedades de controller. Además de omitir los comentarios en la transpilación a Javascript.

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,11 +1,22 @@
-const test = (req:Express.Request, res: { json: (arg0: { status: number; success: boolean; message: string; }) => void; }) => {
+import { Request, Response } from "express";
+
+// Se define el tipo del objeto controller como un objeto que puede tener claves de tipo string
+// y cuyos valores son funciones que toman Request y Response como parámetros
+// y no devuelven nada (void).
+interface Controller { 
+    [key: string]: (req: Request, res: Response) => void
+};
+
+const controller:Controller = {};
+
+// Dado que test es una propiedad de controller y controller implementa la interfaz Controller,
+// el tipo de controller.test se deduce automáticamente
+controller.test = (req, res) => {
     res.json({
         status: 200,
         success: true,
-        message: "Hello World"
+        message: "It's working!"
     });
-}
+};
 
-export default {
-    test
-}
+export default controller;
