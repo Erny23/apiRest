fc7f9cb01022beed5b09e16cc4a99e643e00900b
Ernesto Fava <e.fava13@gmail.com>
2024-12-08 00:33:45 -0400

    CORS
    Uso de CORS, mecanismo basado en cabeceras HTTP que permite a un servidor indicar cualquier dominio, esquema o puerto con un origen distinto del suyo desde el que un navegador debería permitir la carga de recursos.

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
+import corsOptions from "./src/config/cors/cors";
 import morgan from "morgan";
 import routes from "./src/routes/router";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// middlewares
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 
 app.get("*", routes);
