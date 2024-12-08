7d428101da3386b42f2319ccce98a7fcecdac6c7
Ernesto Fava <e.fava13@gmail.com>
2024-12-08 00:35:56 -0400

    Origen dinámico en CORS
    Verifica el origen desde una lista blanca o en este caso la lista de puertos locales.

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import corsOptions from "./src/config/cors/cors";
+import {corsOptions, localOptions} from "./src/config/cors/cors";
 import morgan from "morgan";
 import routes from "./src/routes/router";
 
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // middlewares
-app.use(cors(corsOptions));
+app.use(cors(corsOptions && localOptions));
 app.use(morgan("dev"));
 
 app.get("*", routes);
--- src/config/cors/cors.ts
+++ src/config/cors/cors.ts
@@ -1,6 +1,26 @@
-const corsOptions = {
+// Definición de la lista blanca de orígenes permitidos
+const whitelist: string | string[] = process.env.WHITELIST || [""];
+
+// Opciones de CORS
+/*const corsOptions = {
   origin: 'http://example.com',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};*/
+
+export const corsOptions = {
+  origin: function (origin: string | undefined, callback: (error: Error | null, allow: boolean) => void) {
+    // Condición no segura
+    /* !origin || whitelist.indexOf(origin) !== -1 */
+    if (origin && whitelist.indexOf(origin) !== -1) {
+      callback(null, true); // Permitir el origen
+    } else {
+      callback(new Error('Not allowed by CORS'), false); // No permitir el origen
+    }
+  }
 };
 
-export default corsOptions;
+export const localOptions = {
+  origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
