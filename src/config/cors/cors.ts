16e4066a4d021d9a178df4186ea96d17279cbff6
Ernesto Fava <e.fava13@gmail.com>
2024-12-08 01:53:37 -0400

    Puerto 8080
    Permite solicitudes provenientes del puerto local 8080.

--- src/config/cors/cors.ts
+++ src/config/cors/cors.ts
@@ -20,7 +20,7 @@ export const corsOptions = {
 };
 
 export const localOptions = {
-  origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+  origin: ["http://localhost:3000", "http://127.0.0.1:3000","http://localhost:8080", "http://127.0.0.1:8080"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 };
